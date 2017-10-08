c9bdac18e84a428386a753cd19d0dd10804ff099
Dominika Zdunek <dozdunek@gmail.com>
2017-10-08 20:55:39 +0200

    9.6

--- scripts.js
+++ scripts.js
@@ -1,8 +1,8 @@
-let list = document.getElementById('list');
-let add = document.getElementById('addElem');
+var list = document.getElementById('list');
+var add = document.getElementById('addElem');
 add.addEventListener('click', function() {
-    let element = document.createElement('li');
-    let arrayOfLi = document.getElementsByTagName('Li').length;
+    var element = document.createElement('li');
+    var arrayOfLi = document.getElementsByTagName('Li').length;
     element.innerHTML = `item  ${arrayOfLi}`;
     list.appendChild(element);
 });
